29ebd807ef52a59326a12ebfc1f5242e678c32c8
luke williams <jluke94williams@gmail.com>
2020-02-09 10:32:25 -0500

    Added an Array of Objects used to make up password

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
 // Assignment Code 
 var genRanPass = document.querySelector("#generate")
 
+//Random Password String to be created.
+var passCharString = "";
+
+// Created an Array of Objects. Objects contain the type of Charatrers, Name and Use. 
+var passCharSet = [
+
+    // Object Set 1: Uppercase Letters.
+    
+    {
+        characters:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        name: "Uppercase Letters",
+        use: false,
+    },
+    
+    // Object Set 2: lowercase Letters.
+    
+    {             
+        characters:"abcdefghijklmnopqrstuvwxyz",
+        name: "LowerCase Letters",
+        use: false,
+    },
+    
+    // Object Set 3: Special Characters.
+    
+    {             
+        characters: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
+        name: "Special Characters",
+        use: false
+    },
+    
+    // Object Set 4: Numbers.
+    
+    {
+        characters:"0123456789",
+        name: "Numbers",
+        use: false,
+    }
+]
+
 // Event Listener
 genRanPass.addEventListener("click", generate);
  
